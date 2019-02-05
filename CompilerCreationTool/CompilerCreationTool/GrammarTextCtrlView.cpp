35664fa38d05e021a56c1703e513474cdc394ff5
Timur <chosti34@gmail.com>
2019-02-05 19:44:43 +0300

    Реализованы вкладки для терминалов и действий

--- CompilerCreationTool/CompilerCreationTool/GrammarTextCtrlView.cpp
+++ CompilerCreationTool/CompilerCreationTool/GrammarTextCtrlView.cpp
@@ -2,6 +2,10 @@
 #include "GrammarTextCtrlView.h"
 
 #include <wx/statbox.h>
+#include <wx/notebook.h>
+#include <wx/listctrl.h>
+
+#include <wx/aui/aui.h>
 
 GrammarTextCtrlView::GrammarTextCtrlView(wxWindow* parent)
 	: wxPanel(parent, wxID_ANY)
@@ -10,39 +14,46 @@ GrammarTextCtrlView::GrammarTextCtrlView(wxWindow* parent)
 	wxBoxSizer* hSizer = new wxBoxSizer(wxHORIZONTAL);
 	hSizer->Add(m_textCtrl, 1, wxEXPAND | wxALL, 5);
 
-	wxBoxSizer* vSizer = new wxBoxSizer(wxVERTICAL);
-	hSizer->Add(vSizer, 1, wxEXPAND | wxALL, 5);
+	wxNotebook* notebook = new wxNotebook(this, wxID_ANY, wxDefaultPosition, wxDefaultSize);
+	hSizer->Add(notebook, 1, wxEXPAND | wxALL, 5);
 
-	wxStaticBoxSizer* stSizer1 = new wxStaticBoxSizer(wxVERTICAL, this, wxT("Terminals"));
-	wxListBox* terminalsList = new wxListBox(stSizer1->GetStaticBox(), wxID_ANY, wxDefaultPosition, wxDefaultSize, wxArrayString(), wxLB_ALWAYS_SB);
+	wxPanel* terminalsPanel = new wxPanel(notebook, wxID_ANY);
+	wxBoxSizer* terminalsPanelSizer = new wxBoxSizer(wxVERTICAL);
+
+	wxListBox* terminalsList = new wxListBox(terminalsPanel, wxID_ANY, wxDefaultPosition, wxDefaultSize, wxArrayString(), wxBORDER_SIMPLE);
 	terminalsList->Insert(wxT("string"), 0);
-	stSizer1->Add(terminalsList, 1, wxEXPAND | wxALL, 5);
+	terminalsPanelSizer->Add(terminalsList, 1, wxEXPAND | wxALL, 5);
 
 	// Buttons for terminals list
-	wxButton* addTerminalButton = new wxButton(stSizer1->GetStaticBox(), wxID_ANY, wxT("Add"));
-	wxButton* deleteTerminalButton = new wxButton(stSizer1->GetStaticBox(), wxID_ANY, wxT("Delete"));
+	wxButton* addTerminalButton = new wxButton(terminalsPanel, wxID_ANY, wxT("Add"));
+	wxButton* deleteTerminalButton = new wxButton(terminalsPanel, wxID_ANY, wxT("Delete"));
 
 	wxBoxSizer* terminalButtonsSizer = new wxBoxSizer(wxHORIZONTAL);
 	terminalButtonsSizer->Add(addTerminalButton, 0);
-	terminalButtonsSizer->Add(deleteTerminalButton, 0);
+	terminalButtonsSizer->Add(deleteTerminalButton, 0, wxLEFT, 5);
 
-	stSizer1->Add(terminalButtonsSizer, 0, wxALIGN_RIGHT | wxALL, 5);
+	terminalsPanelSizer->Add(terminalButtonsSizer, 0, wxALIGN_RIGHT | wxALL, 5);
+	terminalsPanel->SetSizer(terminalsPanelSizer);
 
-	wxStaticBoxSizer* stSizer2 = new wxStaticBoxSizer(wxVERTICAL, this, wxT("Actions"));
-	wxListBox* actionsList = new wxListBox(stSizer2->GetStaticBox(), wxID_ANY, wxDefaultPosition, wxDefaultSize, wxArrayString(), wxLB_ALWAYS_SB);
-	stSizer2->Add(actionsList, 1, wxEXPAND | wxALL, 5);
+	wxPanel* actionsPanel = new wxPanel(notebook, wxID_ANY);
+	wxBoxSizer* actionsPanelSizer = new wxBoxSizer(wxVERTICAL);
+
+	wxListBox* actionsList = new wxListBox(actionsPanel, wxID_ANY, wxDefaultPosition, wxDefaultSize, wxArrayString(), wxBORDER_SIMPLE);
+	actionsPanelSizer->Add(actionsList, 1, wxEXPAND | wxALL, 5);
 
 	// Buttons for actions list
-	wxButton* addActionButton = new wxButton(stSizer2->GetStaticBox(), wxID_ANY, wxT("Add"));
-	wxButton* deleteActionButton = new wxButton(stSizer2->GetStaticBox(), wxID_ANY, wxT("Delete"));
+	wxButton* addActionButton = new wxButton(actionsPanel, wxID_ANY, wxT("Add"));
+	wxButton* deleteActionButton = new wxButton(actionsPanel, wxID_ANY, wxT("Delete"));
 
 	wxBoxSizer* actionButtonsSizer = new wxBoxSizer(wxHORIZONTAL);
 	actionButtonsSizer->Add(addActionButton, 0);
-	actionButtonsSizer->Add(deleteActionButton, 0);
+	actionButtonsSizer->Add(deleteActionButton, 0, wxLEFT, 5);
 
-	stSizer2->Add(actionButtonsSizer, 0, wxALIGN_RIGHT | wxALL, 5);
+	actionsPanelSizer->Add(actionButtonsSizer, 0, wxALIGN_RIGHT | wxALL, 5);
+	actionsPanel->SetSizer(actionsPanelSizer);
 
-	vSizer->Add(stSizer1, 1, wxEXPAND);
-	vSizer->Add(stSizer2, 1, wxEXPAND);
+	notebook->AddPage(terminalsPanel, wxT("Terminals"));
+	notebook->AddPage(actionsPanel, wxT("Actions"));
+
 	SetSizerAndFit(hSizer);
 }
